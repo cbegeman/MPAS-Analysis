bbfeac3d6d2d89cea67fb74d034d1361b1f2bdc5
Carolyn Begeman <cbegeman@lanl.gov>
2022-09-22 15:17:16 -0500

    WIP test histograms with ssh climatology

--- mpas_analysis/__main__.py
+++ mpas_analysis/__main__.py
@@ -194,6 +194,8 @@ def build_analysis_list(config, controlConfig):
                                         controlConfig))
     analyses.append(ocean.TimeSeriesTransport(config, controlConfig))
 
+    analyses.append(ocean.HistogramSSH(config, oceanClimatolgyTasks['avg'], oceanRegionMasksTask,
+                                       controlConfig))
     analyses.append(ocean.MeridionalHeatTransport(
         config, oceanClimatolgyTasks['avg'], controlConfig))
 
